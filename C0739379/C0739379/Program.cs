9408eeba7fa9df4894baec1f0e4f5610e501d9b8
Sandeep Singh Dhillon <739379@student.tor.loc>
2019-05-08 10:54:06 -0400

    2nd commit

--- C0739379/C0739379/Program.cs
+++ C0739379/C0739379/Program.cs
@@ -10,6 +10,31 @@ namespace C0739379
     {
         static void Main(string[] args)
         {
+            int n = 1098;
+            Method m = new Method();
+            int sum = m.Result(n);
+
+            Console.WriteLine("Sum is " + sum);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Method
+    {
+        public int Result(int n)
+        {
+            int sum = 0;
+            while (n >= 514)
+            {
+                Console.WriteLine(n);
+                sum = sum + n;
+                n -= 33;
+            }
+
+
+
+            return sum;
         }
     }
 }
